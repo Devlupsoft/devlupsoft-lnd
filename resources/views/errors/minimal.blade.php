f53631c02b54f3e760294d7cf0a7b849b6824ca6
Vinay Kumar <vinaysoftwaredev07@gmail.com>
2024-02-08 23:14:42 +0530

    main

--- resources/views/errors/minimal.blade.php
+++ resources/views/errors/minimal.blade.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>{{@yield('code') | @yield('message')}}</title>
+  <title>{{@yield('code') }} | {{ @yield('message')}}</title>
 
   <!-- Favicons -->
   <link href="{{ asset('front/img/favicon.png') }}" rel="icon">
